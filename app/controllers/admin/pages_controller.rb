1043eac6f50603c8740e65b8967fbb3a401de0da
Sebastian Iorga <si@globalwaterintel.com>
2015-05-20 00:23:44 +0100

    add treeish stuff to admin form

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -91,7 +91,7 @@ module Admin
 
     def page_params
       params.require(:effective_page).permit(
-        :title, :meta_description, :draft, :layout, :template, :slug, :roles => []
+        :title, :meta_description, :draft, :layout, :template, :slug, :show_children, :parent_id, :roles => []
       )
     end
 
